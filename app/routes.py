aa8d4964e734711a5d410d0e1c8ca18f4b57ca93
Anh Huynh <avh2@uw.edu>
2023-04-20 11:34:56 -0700

    Modified routes.py file and created planet Class and planet instances

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Planet:
+    def __init__(self, id, name, description, has_water):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.has_water = has_water
+        
+        
+planet1 = Planet(35, "Mars", "red", True)
+planet2 = Planet(65, "Jupiter", "orange", True)
+planet3 = Planet(1, "Earth", "green", True)
+
+planet_list = [planet1, planet2, planet3]
+
+planet_bp = Blueprint("planet", __name__, url_prefix="/planet")
+
+@planet_bp.route("", methods=["GET"])
+
+def get_planets():
+    response = []
+    for planet in planet_list:
+        planet_dict = {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "has_water": planet.has_water            
+        }
+        
+    response.append(planet_dict)
+    
+    return jsonify(response), 200
 
