ddefff27348d412a563fe1e4ef14ec3da6e49997
Samantha Hurtado <samanthahurtado@Samanthas-MacBook-Pro.local>
2023-04-20 11:43:25 -0700

    modified bluieprint import in create_app

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    from .routes.planet import planet_bp
+    from .routes import planet_bp
     app.register_blueprint(planet_bp)
     return app 
