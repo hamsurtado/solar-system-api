fb188e6b5e6bcbb35eb4b46976b67d315c3f383e
Anh Huynh <avh2@uw.edu>
2023-04-27 13:49:34 -0700

    Modified app/routes.py file to create a planet

--- app/routes.py
+++ app/routes.py
@@ -1,52 +1,70 @@
-from flask import Blueprint, jsonify
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
-class Planet:
-    def __init__(self, id, name, description, has_water):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.has_water = has_water
+
+# class Planet:
+#     def __init__(self, id, name, description, has_water):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.has_water = has_water
         
         
-planet1 = Planet(35, "Mars", "red", True)
-planet2 = Planet(65, "Jupiter", "orange", True)
-planet3 = Planet(1, "Earth", "green", True)
+# planet1 = Planet(35, "Mars", "red", True)
+# planet2 = Planet(65, "Jupiter", "orange", True)
+# planet3 = Planet(1, "Earth", "green", True)
 
-planet_list = [planet1, planet2, planet3]
+# planet_list = [planet1, planet2, planet3]
 
 planet_bp = Blueprint("planet", __name__, url_prefix="/planet")
 
-@planet_bp.route("", methods=["GET"])
-def get_planets():
-    response = []
-    for planet in planet_list:
-        planet_dict = {
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "has_water": planet.has_water            
-        }
-        
-        response.append(planet_dict)
-    
-    return jsonify(response), 200
+@planet_bp.route("", methods=["POST"])
+def add_planet():
+    request_body = request.get_json()
+    new_planet = Planet(name=request_body["name"], 
+                        description=request_body["description"], 
+                        has_water=request_body["has_water"])
 
-@planet_bp.route("/<id>", methods=["GET"])
-def get_one_planet(id):
-    try:
-        planet_id = int(id)
-    except:
-        return {"message": f"invalid id: {id}"}, 400
+    db.session.add(new_planet)
+    db.session.commit()
     
-    for planet in planet_list:
-        if planet_id == planet.id:
-            return {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "has_water": planet.has_water
-            }
+    return make_response(f"Planet {new_planet.name} with id {new_planet.id} successfully created", 201)
+
+
+
+
+# @planet_bp.route("", methods=["GET"])
+# def get_planets():
+#     response = []
+#     for planet in planet_list:
+#         planet_dict = {
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description,
+#             "has_water": planet.has_water            
+#         }
+        
+#         response.append(planet_dict)
     
-    return {"message": f"id {planet_id} not found"}, 404        
+#     return jsonify(response), 200
+
+# @planet_bp.route("/<id>", methods=["GET"])
+# def get_one_planet(id):
+#     try:
+#         planet_id = int(id)
+#     except:
+#         return {"message": f"invalid id: {id}"}, 400
+    
+#     for planet in planet_list:
+#         if planet_id == planet.id:
+#             return {
+#                 "id": planet.id,
+#                 "name": planet.name,
+#                 "description": planet.description,
+#                 "has_water": planet.has_water
+#             }
+    
+#     return {"message": f"id {planet_id} not found"}, 404        
         
 
