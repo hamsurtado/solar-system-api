613726841e7116631f5478001e574f46de02a76f
Anh Huynh <avh2@uw.edu>
2023-04-27 13:21:05 -0700

    Modified app/init file and created models/init file and models/planet.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-def create_app():
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(test_config=None):
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
     from .routes import planet_bp
     app.register_blueprint(planet_bp)
+    
     return app 
