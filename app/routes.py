489622f8b970ecc94ea23d68679e88e2103e5b68
Anh Huynh <avh2@uw.edu>
2023-05-01 14:29:53 -0700

    Modified routes.py file and added a helper function validate_planet

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import db
 from app.models.planet import Planet
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 
 
 # class Planet:
@@ -42,10 +42,7 @@ def read_all_planets():
 
 @planet_bp.route("/<planet_id>", methods=["GET"])
 def get_one_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        return {"message": f"invalid id: {planet_id}"}, 400
+    planet = validate_planet(planet_id)
     
     planet = Planet.query.get_or_404(planet_id)
     
@@ -53,12 +50,10 @@ def get_one_planet(planet_id):
         
 @planet_bp.route("/<planet_id>", methods=["PUT"])
 def update_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        return {"message": f"invalid id: {planet_id}"}, 400
-    
+    planet = validate_planet(planet_id)
+
     planet = Planet.query.get_or_404(planet_id)
+    
     request_data = request.get_json()
 
     planet.name = request_data["name"]
@@ -70,14 +65,17 @@ def update_planet(planet_id):
 
 @planet_bp.route("/<planet_id>", methods=["DELETE"])
 def delete_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        return {"message": f"invalid id: {planet_id}"}, 400
-    
+    planet = validate_planet(planet_id)
+
     planet = Planet.query.get_or_404(planet_id)
+    
     db.session.delete(planet)
     db.session.commit()
 
     return {"mssg": f"planet {planet_id} succesfully annihilated"}, 200
-    
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except:
+        abort(make_response({"message":f"planet {planet_id} invalid"}, 400))
